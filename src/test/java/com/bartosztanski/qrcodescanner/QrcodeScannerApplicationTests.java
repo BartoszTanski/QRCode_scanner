f8d0d6a0ea539806a24e31f6be8deda39a5b31bd
Bartosz Tanski <b.t4nsky@gmail.com>
2023-08-29 11:52:28 +0200

    base from initialzr

---
+++ src/test/java/com/bartosztanski/qrcodescanner/QrcodeScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartosztanski.qrcodescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
